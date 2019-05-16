88b1a0a2fe5e34d2f57ea78b911f63ae4af0825d
=tumoqun <=nguyenanhtukhtn@gmail.com>
2019-05-16 14:48:46 +0700

    nothing

--- CafeShop/app/src/main/java/com/example/cafeshop/View/DangNhap.java
+++ CafeShop/app/src/main/java/com/example/cafeshop/View/DangNhap.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -66,6 +67,7 @@ public class DangNhap extends AppCompatActivity implements View.OnClickListener,
         edPassword = (EditText) findViewById(R.id.Password);
 
 
+
         btndangki.setOnClickListener(this);
         btnDangNhapFacbook=(LoginButton) findViewById(R.id.FacebookLogin);
         btnDangNhapFacbook.setReadPermissions("email","public_profile");
@@ -74,8 +76,16 @@ public class DangNhap extends AppCompatActivity implements View.OnClickListener,
     }
 
     private void DangNhap(){
-                String Email=edEmail.getText().toString();
-                String Password=edPassword.getText().toString();
+        String Email=edEmail.getText().toString().trim();
+        String Password=edPassword.getText().toString().trim();
+                if (TextUtils.isEmpty(Email)) {
+                    Toast.makeText(DangNhap.this,"Vui lòng nhập Email!",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(Password)){
+                    Toast.makeText(DangNhap.this,"Vui lòng nhập mật khẩu!",Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 firebaseAuth.signInWithEmailAndPassword(Email,Password).addOnCompleteListener( new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
