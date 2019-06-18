f2d950b468b840a67a8d0e4eb3badcc0920eb950
=tumoqun <=nguyenanhtukhtn@gmail.com>
2019-06-18 16:19:57 +0700

    thêm lại bình luận

---
+++ CafeShop/app/src/main/java/com/example/cafeshop/View/TrangChu.java
@@ -0,0 +1,31 @@
+package com.example.cafeshop.View;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.cafeshop.Adapters.AdapterViewPagerTrangChu;
+import com.example.cafeshop.R;
+
+public class TrangChu  extends AppCompatActivity {
+  //  Intent recv;
+    ViewPager viewPagerTrangChu;
+    public  void onCreate (@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trangchu);
+  //      recv=getIntent();
+
+        //Tìm viewpagerTrangChu ở trang chủ gán vào viewPagerTrangChu
+        viewPagerTrangChu=(ViewPager)findViewById(R.id.viewpager_TrangChu);
+        //Khởi tạo adapter cho viewpager
+//        AdapterViewPagerTrangChu adapterViewPagerTrangChu= new AdapterViewPagerTrangChu(getSupportFragmentManager());
+        AdapterViewPagerTrangChu adapterViewPagerTrangChu = new AdapterViewPagerTrangChu((getSupportFragmentManager()));
+        //Gắn adapter vào viewPagerTrangChu vừa khai báo.
+        viewPagerTrangChu.setAdapter(adapterViewPagerTrangChu);
+    }
+
+
+}
